2e8c78bb3531726aeac8de020ea2788c344d25ee
manojnaik24 <chittinaik24@gmail.com>
2023-12-12 16:25:41 +0530

    create

--- src/main/java/com/registration/RegistartionServlet.java
+++ src/main/java/com/registration/RegistartionServlet.java
@@ -1,11 +1,12 @@
 package com.registration;
 
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,20 +15,47 @@ import jakarta.servlet.http.HttpServletResponse;
 //servlet implementation class registartion
 
 @WebServlet("/register")
-public class RegistartionServlet extends HttpServlet{
-	
+public class RegistartionServlet extends HttpServlet {
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	   String uname=request .getParameter("name");
-	   String umail=request .getParameter("name");
-	   String upwd=request .getParameter("name");
-	   String umobile=request .getParameter("name");
-    PrintWriter out=((ServletResponse) request).getWriter();
-    out.print(uname);
-    out.print(umail);
-    out.print(upwd);
-    out.print(umobile);
+	   String uemail=request .getParameter("email");
+	   String upwd=request .getParameter("pass");
+	   String umobile=request .getParameter("contact");
+	   RequestDispatcher dispacther=null;
+	   Connection con=null;
+    try {
+    	class.forName("com.mysql.cj.jdbc.Driver");
+    	DriverManager.getConnection("jdbc:mysql://localhost:3360/youtube","root","root");
+    	PreparedStatement pst = con.
+    	prepareStatement("insert into users (uname, upwd, uemail, umobile) values (?,?,?,?) ");
+    	pst.setString(1, uname);
+    	pst.setString(2, upwd);
+    	pst.setString(3, uemail);
+    	pst.setString(4, umobile);
+    	
+    	int rowCount = pst.executeUpdate();
+    	dispacther=request.getRequestDispatcher("registeration.jsp");
+    	if (rowCount >0)
+    	{
+    		request.setAttribute("status", "sucess");
+    	}else {
+    		request.setAttribute("status", "failed");
+    	}
+    	dispacther.forward(request, response);
+    	
+    }catch(Exception  e){
+    	e.printStackTrace();
+    }finally {
+    	try {
+    		con.close();
+    	}catch (Exception e) {
+    		e.printStackTrace();
+		}
+    }
 
-
-	}
-
+    
+    
+	
+    }
 }
